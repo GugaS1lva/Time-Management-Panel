28bf573931f10dac5f56bc3922c781fb814d173c
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-07-01 13:21:57 -0300

    Testes de Rotas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 function Home(){
-    return <h1>Olá, DNV!</h1>
+    return (
+        <>
+            <h1>HOME</h1>
+
+            <Link href="/sobre">
+                <a>Acessar SOBRE</a>
+            </Link>
+        </>
+    )
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+function Sobre(){
+    return (
+        <>
+            <h1>SOBRE</h1>
+
+            <Link href="/">
+                <a>Voltar para Home</a>
+            </Link>
+        </>
+    )
+}
+
+export default Sobre
